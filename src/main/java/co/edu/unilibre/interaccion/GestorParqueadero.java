9c02ea5b425d0f62e6a852817df8f4ac0ad9c92f
DGZamudio <diegozam16@hotmail.com>
2026-09-20 15:49:36 -0500

    Feat: Pruebas unitarias de registrar moto implementada

--- src/main/java/co/edu/unilibre/interaccion/GestorParqueadero.java
+++ src/main/java/co/edu/unilibre/interaccion/GestorParqueadero.java
@@ -1,25 +1,35 @@
 package co.edu.unilibre.interaccion;
 
+import java.util.ArrayList;
+
 import co.edu.unilibre.datos.Moto;
+import co.edu.unilibre.datos.Parqueadero;
 
 /**
  * GestorParqueadero
  */
 public class GestorParqueadero {
 
-	public boolean registrarEntradaMoto(Moto moto) {
+	public boolean registrarEntradaMoto(Parqueadero pq, Moto moto) {
+	    ArrayList<Moto> espacios = pq.obtenerMotos();
+		if (espacios.size() == 23) {
+		    return false;
+		}
 
+		espacios.add(moto);
+
+		return true;
 	}
 
 	public boolean registrarSalidaMoto(String placa) {
-
+	    return true;
 	}
 
 	public boolean registrarPago() {
-
+	    return true;
 	}
 
 	public boolean generarReporte() {
-
+	    return true;
 	}
 }
--- src/test/java/co/edu/unilibre/interaccion/TestGestorParqueadero.java
+++ src/test/java/co/edu/unilibre/interaccion/TestGestorParqueadero.java
@@ -1,8 +1,12 @@
 package co.edu.unilibre.interaccion;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import co.edu.unilibre.datos.Moto;
 import co.edu.unilibre.datos.Parqueadero;
 
 /**
@@ -21,12 +25,36 @@ public class TestGestorParqueadero {
 
     @Test
 	public void registrarEntradaMotoOk() {
-
+	    Moto mt = new Moto("ABC 123", "132432564");
+	    boolean res = gestor.registrarEntradaMoto(parqueadero, mt);
+		assertTrue(res);
 	}
 
 	@Test
     public void registrarEntradaMotoFail() {
+        String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < 23; i++) {
+            // Generar placas diferentes
+            String placa = "";
+            for (int j = 0; j < 3; j++) {
+                placa += letras.charAt((i + j) % letras.length());
+            }
+            placa += " ";
+            for (int j = 0; j < 3; j++) {
+                placa += (i + j) % 10;
+            }
 
+            // Generar cedulas diferentes
+            String cedula = "";
+            for (int j = 0; j < 9; j++) {
+                cedula += (i + j) % 10;
+            }
+            Moto mt = new Moto(placa, cedula);
+            gestor.registrarEntradaMoto(parqueadero, mt);
+        }
+        Moto mt = new Moto("ZZZ 777", "192766656");
+        boolean res = gestor.registrarEntradaMoto(parqueadero, mt);
+        assertFalse(res);
     }
 
 	@Test
